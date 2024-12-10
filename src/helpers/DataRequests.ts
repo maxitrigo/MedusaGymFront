add98ae971c27ca9aa999e5d4c089a1c2e9b7792
maxitrigo <maxitrigo@gmail.com>
2024-12-10 19:34:39 -0300

    cambio en la ruta auth

--- src/helpers/DataRequests.ts
+++ src/helpers/DataRequests.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { v4 as uuidv4 } from 'uuid';
 
-const microAuth = 'http://18.231.148.87'
+const microAuth = 'http://backend:3001'
 const microTrans = 'http://localhost:3000'
 const microGym = 'http://localhost:3005'
 
