68d6dde059d8425acb42283d8e855d9e290634cf
maxitrigo <maxitrigo@gmail.com>
2024-12-12 17:19:54 -0300

    agregar ver contrasenia

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -4,37 +4,48 @@ import { useNavigate, useParams } from "react-router-dom";
 import { login } from "../Redux/userSlice";
 import { setGymInfo } from "../Redux/gymSlice";
 import { addUserToGym, checkLogin, getGymInfo, userRegister } from "../helpers/DataRequests";
+import { GoEye, GoEyeClosed } from "react-icons/go";
 
 export const Register: React.FC = () => {
     const { gymSlug }: any = useParams();
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const [userData, setUserData] = useState({
         email: "",
         name: "",
         surname: "",
-        password: ""
-    })
+        password: "",
+        confirmPassword: ""
+    });
+
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
     const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = event.target
+        const { name, value } = event.target;
         setUserData({
             ...userData,
             [name]: value
-        })
-    }
+        });
+    };
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-    
+
         // Verificar que todos los campos estén llenos
         const allFieldsFilled = Object.values(userData).every((value) => value.trim() !== "");
         if (!allFieldsFilled) {
             alert("Por favor, completa todos los campos.");
             return;
         }
-    
+
+        // Verificar que las contraseñas coincidan
+        if (userData.password !== userData.confirmPassword) {
+            alert("Las contraseñas no coinciden.");
+            return;
+        }
+
         sessionStorage.setItem('slug', gymSlug);
         try {
             const response = await userRegister(userData);
@@ -53,14 +64,14 @@ export const Register: React.FC = () => {
                 navigate('/');
                 return;
             }
-    
+
             // Intentar recuperar información del gimnasio
             let gymInfo = await getGymInfo();
             if (!gymInfo) {
                 sessionStorage.setItem('slug', 'default');
                 gymInfo = await getGymInfo();
             }
-    
+
             if (gymInfo) {
                 const addUser = await addUserToGym(gymInfo.gymToken);
                 if (addUser) {
@@ -71,21 +82,67 @@ export const Register: React.FC = () => {
                     }
                 }
             }
-    
+
             alert('Error al agregar el usuario al gimnasio.');
         } catch (error) {
             console.log('Error:', error);
         }
     };
-    
 
     return (
         <div className="">
             <form onSubmit={handleSubmit} className="flex flex-col gap-2 items-center">
-                <input onChange={handleInput} name="email" className=" rounded-2xl outline-none border-none py-1 px-4" type="text" placeholder="Email" />
-                <input onChange={handleInput} name="name" className=" rounded-2xl outline-none border-none py-1 px-4" type="text" placeholder="Nombre" />
-                <input onChange={handleInput} name="surname" className=" rounded-2xl outline-none border-none py-1 px-4" type="text" placeholder="Apellido" />
-                <input onChange={handleInput} name="password" className=" rounded-2xl outline-none border-none py-1 px-4" type="password" placeholder="Contraseña" />
+                <input 
+                    onChange={handleInput} 
+                    name="email" 
+                    className="rounded-2xl outline-none border-none py-1 px-4" 
+                    type="text" 
+                    placeholder="Email" 
+                />
+                <input 
+                    onChange={handleInput} 
+                    name="name" 
+                    className="rounded-2xl outline-none border-none py-1 px-4" 
+                    type="text" 
+                    placeholder="Nombre" 
+                />
+                <input 
+                    onChange={handleInput} 
+                    name="surname" 
+                    className="rounded-2xl outline-none border-none py-1 px-4" 
+                    type="text" 
+                    placeholder="Apellido" 
+                />
+                <div className="relative w-full">
+                    <input 
+                        onChange={handleInput} 
+                        name="password" 
+                        className="rounded-2xl outline-none border-none py-1 px-4 w-full" 
+                        type={showPassword ? "text" : "password"} 
+                        placeholder="Contraseña" 
+                    />
+                    <span 
+                        onClick={() => setShowPassword(!showPassword)} 
+                        className="absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                    >
+                        {showPassword ? <GoEyeClosed className="text-zinc-400" /> : <GoEye className="text-zinc-400" />}
+                    </span>
+                </div>
+                <div className="relative w-full">
+                    <input 
+                        onChange={handleInput} 
+                        name="confirmPassword" 
+                        className="rounded-2xl outline-none border-none py-1 px-4 w-full" 
+                        type={showConfirmPassword ? "text" : "password"} 
+                        placeholder="Confirmar contraseña" 
+                    />
+                    <span 
+                        onClick={() => setShowConfirmPassword(!showConfirmPassword)} 
+                        className="absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                    >
+                        {showConfirmPassword ? <GoEyeClosed className="text-zinc-400" /> : <GoEye className="text-zinc-400" />}
+                    </span>
+                </div>
                 <button className="button-primary">Registrarse</button>
             </form>
         </div>
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,43 +1,49 @@
 import { useState } from "react";
-import { useNavigate, useParams} from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { login, logout } from "../Redux/userSlice";
 import { useDispatch } from "react-redux";
 import { setGymInfo } from "../Redux/gymSlice";
 import { checkLogin, checkOwnership, getGymInfo, userLogin } from "../helpers/DataRequests";
+import { GoEye, GoEyeClosed } from "react-icons/go";
 
 export const SignIn: React.FC = () => {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
     const { gymSlug }: any = useParams();
-    
+
     const [userData, setUserData] = useState({
         email: '',
         password: ''
-    })
+    });
+
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-
-        const { name, value } = event.target
+        const { name, value } = event.target;
 
         setUserData({
             ...userData,
             [name]: value
-        })
-    }
-    
+        });
+    };
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         sessionStorage.setItem('slug', gymSlug);
-    
+
         try {
             const storedSlug = sessionStorage.getItem('slug');
-    
+
             if (!storedSlug || storedSlug === 'auth' || storedSlug === 'undefined') {
                 alert("Por favor selecciona una academia");
                 navigate('/default');
                 return;
             }
-    
+
             const loginInfo = await userLogin(userData);
             const user = {
                 token: loginInfo.token,
@@ -45,13 +51,13 @@ export const SignIn: React.FC = () => {
                 name: loginInfo.name,
                 email: loginInfo.email,
             };
-    
+
             await dispatch(login(user));
-    
+
             if (user.role === 'admin') {
                 const gymInfo = await getGymInfo();
                 await dispatch(setGymInfo(gymInfo));
-    
+
                 const response = await checkOwnership();
                 if (!response) {
                     dispatch(logout());
@@ -60,7 +66,7 @@ export const SignIn: React.FC = () => {
                 }
                 return navigate(`/${gymSlug}/home`);
             }
-    
+
             const gymInfo = await getGymInfo();
             if (gymInfo) {
                 await dispatch(setGymInfo(gymInfo));
@@ -69,8 +75,8 @@ export const SignIn: React.FC = () => {
                     dispatch(logout());
                     alert('No tienes permiso para ingresar a esta academia. Para registrarte en ella primero des asocia tu academia actual desde tu perfil, y luego ingresa a tu nueva academia!');
                     return;
-                } else if ( response.slug === 'default' ) {
-                    alert('Fuiste desvinculado de tu academia, si crees que este mensaje es un error comunicate con la misma!, mientras tanto podes iniciar sesion en la academia generica!')
+                } else if (response.slug === 'default') {
+                    alert('Fuiste desvinculado de tu academia, si crees que este mensaje es un error comunicate con la misma!, mientras tanto podes iniciar sesion en la academia generica!');
                     sessionStorage.setItem('slug', 'default');
                     const defaultGymInfo = await getGymInfo();
                     await dispatch(setGymInfo(defaultGymInfo));
@@ -81,7 +87,7 @@ export const SignIn: React.FC = () => {
                 }
                 return navigate(`/${gymSlug}/home`);
             }
-    
+
             sessionStorage.setItem('slug', 'default');
             const defaultGymInfo = await getGymInfo();
             await dispatch(setGymInfo(defaultGymInfo));
@@ -89,24 +95,43 @@ export const SignIn: React.FC = () => {
             if (response) {
                 return navigate(`/default/home`);
             }
-    
+
             dispatch(logout());
             alert('No se pudo iniciar sesión en el gimnasio predeterminado.');
-    
+
         } catch (error) {
             alert("Email o contraseña incorrectos");
             console.log(error);
         }
     };
-    
 
     return (
         <div>
             <form onSubmit={handleSubmit} className="flex flex-col gap-2 items-center">
-                <input onChange={handleInput} name="email" className="rounded-2xl outline-none border-none py-1 px-4" type="text" placeholder="Email"/>
-                <input onChange={handleInput} name="password" className="rounded-2xl outline-none border-none py-1 px-4" type="password" placeholder="Contraseña"/>
+                <input
+                    onChange={handleInput}
+                    name="email"
+                    className="rounded-2xl outline-none border-none py-1 px-4"
+                    type="text"
+                    placeholder="Email"
+                />
+                <div className="relative w-full">
+                    <input
+                        onChange={handleInput}
+                        name="password"
+                        className="rounded-2xl outline-none border-none py-1 px-4 w-full"
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Contraseña"
+                    />
+                    <span
+                        onClick={togglePasswordVisibility}
+                        className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                    >
+                        {showPassword ? <GoEyeClosed className="text-zinc-400" /> : <GoEye className="text-zinc-400" />}
+                    </span>
+                </div>
                 <button className="button-primary">Ingresar</button>
             </form>
         </div>
-    )
-}
+    );
+};
