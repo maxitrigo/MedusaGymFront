c6908ade5431315dbd8ec3a577879024e6d17adf
maxitrigo <maxitrigo@gmail.com>
2024-12-13 10:34:02 -0300

    qr scanner

--- src/components/QrScanner.tsx
+++ src/components/QrScanner.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { Html5QrcodeScanner } from 'html5-qrcode';
+import { Html5Qrcode, Html5QrcodeScanner } from 'html5-qrcode';
 import { authInfo, logTraining } from '../helpers/DataRequests';
 import { useDispatch } from 'react-redux';
 import { setGymUser } from '../Redux/gymUserSlice';
@@ -7,15 +7,14 @@ import ConfirmationCircle from './ConfirmationCircle';
 
 const QRScanner: React.FC = () => {
   const [confirmed, setConfirmed] = useState<boolean>(false);
-  const [scannerUsed, setScannerUsed] = useState<boolean>(false); // Nuevo estado
+  const [scannerUsed, setScannerUsed] = useState<boolean>(false);
   const qrCodeRef = useRef<HTMLDivElement>(null);
   const dispatch = useDispatch();
 
   const handleScan = async (decodedText: string) => {
     try {
       let result;
-      
-      // Si el QR escaneado contiene una acción esperada, llamamos a la función correspondiente
+
       if (decodedText === 'log-training') {
         const { token } = authInfo();
         result = await logTraining(token);
@@ -27,66 +26,53 @@ const QRScanner: React.FC = () => {
       } else {
         console.warn('Acción desconocida en el QR');
       }
-  
-      if (result && !result.error) {  // Si no hay error en el resultado
+
+      if (result && !result.error) {
         dispatch(setGymUser(result));
         setConfirmed(true);
-  
-        // Esperamos 2 segundos y luego recargamos la página
-        // setTimeout(() => {
-        //   window.location.reload();
-        // }, 3000);
       } else {
-        setConfirmed(false);  // Si hay error, confirmación en false
+        setConfirmed(false);
       }
-  
     } catch (error) {
       console.error('Error al procesar el QR:', error);
-      setConfirmed(false);  // Confirmación en false en caso de error
+      setConfirmed(false);
     } finally {
-        // Siempre se marca el scanner como usado
-        setScannerUsed(true);
-        // Esperamos 2 segundos y luego recargamos la página
-        // setTimeout(() => {
-        //     window.location.reload();
-        // }, 3000);
+      setScannerUsed(true);
     }
   };
-  
 
   useEffect(() => {
-    if (qrCodeRef.current && !scannerUsed) { // Solo renderizamos el escáner si no ha sido usado
-      const html5QrCodeScanner = new Html5QrcodeScanner(
-        'qr-reader', // ID del contenedor
-        {
-          fps: 40, // Frames por segundo
-          qrbox: { width: 150, height: 150 }, // Tamaño del cuadro de escaneo
-        },
-        false
-      );
-  
-      // Renderizamos el escáner
-      html5QrCodeScanner.render(
-        (decodedText) => {
-          handleScan(decodedText).finally(() => {
-            html5QrCodeScanner.clear(); // Limpiar el escáner después de procesar
-          });
-        },
-        () => {
-        
-        }
-      );
+    if (qrCodeRef.current && !scannerUsed) {
+      const html5QrCode = new Html5Qrcode('qr-reader'); // Usamos Html5Qrcode directamente
+      html5QrCode
+        .start(
+          { facingMode: 'environment' }, // Cámara trasera
+          {
+            fps: 40,
+            qrbox: { width: 150, height: 150 },
+          },
+          handleScan, // Callback para procesar el texto del QR
+          () => {
+          }
+        )
+        .catch((err) => console.error('Error al iniciar cámara:', err));
     }
 
-  }, [scannerUsed]); // Dependencia en `scannerUsed`
-  
+    return () => {
+      Html5Qrcode.getCameras().then((cameras) => {
+        if (cameras.length > 0) {
+          console.log("Cámaras detectadas:", cameras);
+        }
+      })
+    };
+  }, [scannerUsed]);
 
   return (
     <div className='bg-background w-[320px] text-white'>
       {
-          !scannerUsed ? 
-            <div id="qr-reader" ref={qrCodeRef}></div> : 
-            <ConfirmationCircle confirmed={confirmed} />
+        !scannerUsed ? 
+        <div id="qr-reader" ref={qrCodeRef}></div> : 
+        <ConfirmationCircle confirmed={confirmed} />
       }
     </div>
   );
