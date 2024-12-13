92e8cb8468732f36fbe5c3d2fa1099ecc141b322
maxitrigo <maxitrigo@gmail.com>
2024-12-13 12:05:38 -0300

    scanner

--- src/views/Qr/qrScann.tsx
+++ src/views/Qr/qrScann.tsx
@@ -1,51 +1,17 @@
-import { useState } from "react";
-import { IoMdQrScanner, IoMdClose } from "react-icons/io";
-import { NavBar } from "../../components/NavBar";
-import QRGenerator from "../../components/QrGenerator";
-import QRScanner from "../../components/QrScanner";
+import { NavBar } from "../../components/NavBar"
+import QRGenerator from "../../components/QrGenerator"
+import QRScanner from "../../components/QrScanner"
 
 export const QrScann = () => {
-  const [isScannerVisible, setIsScannerVisible] = useState(false);
-
-  const handleToggleScanner = () => {
-    setIsScannerVisible(!isScannerVisible);
-  };
-
-  return (
-    <div className="w-full pb-60 bg-white">
-      <NavBar />
-
-      {/* Botón para mostrar el QRScanner */}
-      {!isScannerVisible ? (
-        <div className="vertical-center m-auto h-[400px]">
-            <p className="font-bold italic">Haz click, para escanear el QR</p>
-          <button
-            className="text-10xl text-zinc-900 clickable"
-            onClick={handleToggleScanner}
-          >
-            <IoMdQrScanner />
-          </button>
+    return (
+        <div className="w-full pb-20 bg-white">
+            <NavBar/>
+            <div className="vertical-center w-full">
+                <QRScanner/>
+            </div>
+            <div className="vertical-center w-full bg-white">
+                <QRGenerator/>
+            </div>
         </div>
-      ) : (
-        // Mostrar QRScanner y el botón para cerrar
-        <div className="vertical-center w-full">
-          <QRScanner />
-          <div className="flex justify-center mt-4">
-            <button
-              className="text-4xl p-2 rounded-2xl bg-red-500 text-white"
-              onClick={handleToggleScanner}
-            >
-              <IoMdClose />
-            </button>
-          </div>
-        </div>
-      )}
-
-      {/* Componente QRGenerator */}
-      <div className="vertical-center w-full bg-white mt-4">
-        <QRGenerator />
-      </div>
-    </div>
-  );
-};
-
+    )
+}
