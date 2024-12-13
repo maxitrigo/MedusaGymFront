f34a722a53346a5bef1f07bd5e7fcebc6258f08c
maxitrigo <maxitrigo@gmail.com>
2024-12-13 16:07:13 -0300

    scanner

--- src/views/Qr/qrScann.tsx
+++ src/views/Qr/qrScann.tsx
@@ -13,7 +13,7 @@ export const QrScann = () => {
                 </div>
 
                 {/* QRGenerator desplazándose hacia arriba con el scroll */}
-                <div className="mt-[550px] relative bg-white rounded-tl-3xl rounded-tr-3xl ">
+                <div className="mt-[500px] relative bg-white rounded-tl-3xl rounded-tr-3xl ">
                     <QRGenerator />
                 </div>
             </div>
