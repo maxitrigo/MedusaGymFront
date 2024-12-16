0a3e7c7998edbcea78119904850cd02f4927b5e3
maxitrigo <maxitrigo@gmail.com>
2024-12-16 16:47:57 -0300

    scanner

--- src/components/QrScanner.tsx
+++ src/components/QrScanner.tsx
@@ -4,39 +4,28 @@ import { useDispatch } from "react-redux";
 import { setGymUser } from "../Redux/gymUserSlice";
 import ConfirmationCircle from "./ConfirmationCircle";
 
-// import { Html5QrcodeScanner } from "html5-qrcode"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 const QRScanner = () => {
   const [isScanned, setIsScanned] = useState<boolean>(false)
   const [confirmed, setConfirmed] = useState<boolean>(false);
   const dispatch = useDispatch();
+  const scannerRef = useRef<Html5Qrcode|null> (null)
 
   useEffect(() => {
-    // const scanner = new Html5QrcodeScanner('reader', {
-    //   qrbox: {
-    //     width: 250,
-    //     height: 250,
-    //   },
-    //   fps: 40,
-    // }, false )
-    
-    // const error = () => {
+      startScanner()
+  },[])
 
-    // }
-    // const success = (result: any) => {
-    //   scanner.pause(true)
-    //   scanner.clear()
-    //   setScannResult(result)
-    //   handleScann(scannResult)
-    // }
-
-    // scanner.render(success,error)
-
+  const startScanner = () =>{
+    if (scannerRef.current) {
+      console.log('escaner ya iniciado');
+      
+      return
+    }
     const scanner = new Html5Qrcode('reader')
-    // const cameras = Html5Qrcode.getCameras()
+    scannerRef.current = scanner
     scanner.start(
-      {facingMode: 'user'}, 
+      {facingMode: 'environment'}, 
       {
       qrbox: {
         width: 250,
@@ -44,28 +33,30 @@ const QRScanner = () => {
       },
       fps: 40,
       },
-      (result)=>{
+      (result) => {
         scanner.pause(true)
         handleScann(result)
         setIsScanned(true)
+        scanner.stop()
       },
       () => {}
     )
+  }
 
 
-  },[])
+
+
 
   const handleScann = async (data: any) => {
     try {
             let result;
-            console.log();
-            
+
             if (data === "log-training") {
               const { token } = authInfo();
               result = await logTraining(token);
             } else if (data) {
               const token = data;
-              result = await logTraining(token);
+              result = await logTraining(token)
             }
       
             if (result && !result.error) {
