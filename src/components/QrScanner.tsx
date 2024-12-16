0d99d6fcdd3155c046a9d546a76a9fb27030bff6
maxitrigo <maxitrigo@gmail.com>
2024-12-16 17:56:31 -0300

    scanner

--- src/components/QrScanner.tsx
+++ src/components/QrScanner.tsx
@@ -1,83 +1,98 @@
 import { Html5Qrcode } from "html5-qrcode";
+import { useRef, useEffect, useState } from "react";
 import { authInfo, logTraining } from "../helpers/DataRequests";
 import { useDispatch } from "react-redux";
 import { setGymUser } from "../Redux/gymUserSlice";
 import ConfirmationCircle from "./ConfirmationCircle";
 
-import { useEffect, useRef, useState } from "react"
-
-const QRScanner = () => {
-  const [isScanned, setIsScanned] = useState<boolean>(false)
+const QRScanner = ({ isActive }: { isActive: boolean }) => {
+  const [isScanned, setIsScanned] = useState<boolean>(false);
   const [confirmed, setConfirmed] = useState<boolean>(false);
+  const scannerRef = useRef<Html5Qrcode | null>(null);
   const dispatch = useDispatch();
-  const scannerRef = useRef<Html5Qrcode|null> (null)
 
   useEffect(() => {
-      startScanner()
-  },[])
-
-  const startScanner = () =>{
-    if (scannerRef.current) {
-      console.log('escaner ya iniciado');
-      
-      return
+    if (isActive) {
+      startScanner();
+    } else {
+      stopScanner();
     }
-    const scanner = new Html5Qrcode('reader')
-    scannerRef.current = scanner
+
+    return () => {
+      stopScanner();
+    };
+  }, [isActive]);
+
+  const startScanner = () => {
+    if (scannerRef.current) {
+      return;
+    }
+
+    const scanner = new Html5Qrcode("reader");
+    scannerRef.current = scanner;
+
     scanner.start(
-      {facingMode: 'environment'}, 
+      { facingMode: "environment" },
       {
-      qrbox: {
-        width: 250,
-        height: 250,
-      },
-      fps: 40,
+        qrbox: {
+          width: 250,
+          height: 250,
+        },
+        fps: 40,
       },
       (result) => {
-        scanner.pause(true)
-        handleScann(result)
-        setIsScanned(true)
-        scanner.stop()
+        scanner.pause(true);
+        handleScann(result);
+        setIsScanned(true);
+        stopScanner();
       },
       () => {}
-    )
-  }
+    );
+  };
 
+  const stopScanner = () => {
+    if (scannerRef.current) {
+      scannerRef.current
+        .stop()
+        .then(() => {
+          scannerRef.current = null;
+        })
+        .catch((err) => console.error("Error al detener el escáner:", err));
+    }
+  };
 
-
-
-
-  const handleScann = async (data: any) => {
+  const handleScann = async (data: string) => {
     try {
-            let result;
+      let result;
 
-            if (data === "log-training") {
-              const { token } = authInfo();
-              result = await logTraining(token);
-            } else if (data) {
-              const token = data;
-              result = await logTraining(token)
-            }
-      
-            if (result && !result.error) {
-              dispatch(setGymUser(result));
-              setConfirmed(true);
-            } else {
-              setConfirmed(false);
-            }
-          } catch (error) {
-            setConfirmed(false);
-          }
-  }
+      if (data === "log-training") {
+        const { token } = authInfo();
+        result = await logTraining(token);
+      } else if (data) {
+        const token = data;
+        result = await logTraining(token);
+      }
+
+      if (result && !result.error) {
+        dispatch(setGymUser(result));
+        setConfirmed(true);
+      } else {
+        setConfirmed(false);
+      }
+    } catch (error) {
+      setConfirmed(false);
+    }
+  };
 
   return (
     <div>
-      { isScanned ?
-      <ConfirmationCircle confirmed={confirmed} /> :
-      <div id='reader'></div>
-    }
+      {isScanned ? (
+        <ConfirmationCircle confirmed={confirmed} />
+      ) : (
+        <div id="reader"></div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default QRScanner
+export default QRScanner;
--- src/views/Qr/qrScann.tsx
+++ src/views/Qr/qrScann.tsx
@@ -1,23 +1,33 @@
+import { useEffect, useState } from "react";
 import { NavBar } from "../../components/NavBar";
 import QRGenerator from "../../components/QrGenerator";
 import QRScanner from "../../components/QrScanner";
 
-
 export const QrScann = () => {
-    return (
-        <div className="w-full overflow-hidden overscroll-none">
-            <NavBar />
-            <div className="relative w-full h-full">
-                {/* QRScanner fijo en la pantalla */}
-                <div className="fixed top-0 left-0 w-full h-full">
-                    <QRScanner/>
-                </div>
+  const [scannerActive, setScannerActive] = useState<boolean>(false);
+  useEffect(()=>{
+    setScannerActive(true)
+    return () => {
+        setScannerActive(false)
+    }
+  },[])
 
-                {/* QRGenerator desplazándose hacia arriba con el scroll */}
-                <div className="mt-[500px] relative bg-white rounded-tl-3xl rounded-tr-3xl ">
-                    <QRGenerator />
-                </div>
-            </div>
+  return (
+    <div className="w-full overflow-hidden overscroll-none">
+      <NavBar />
+      <div className="relative w-full h-full">
+        {/* QRScanner fijo en la pantalla */}
+        <div className="fixed top-0 left-0 w-full h-full">
+          <QRScanner isActive={scannerActive} />
         </div>
-    );
+
+        {/* QRGenerator desplazándose hacia arriba con el scroll */}
+        <div className="mt-[500px] relative bg-white rounded-tl-3xl rounded-tr-3xl ">
+          <QRGenerator />
+        </div>
+      </div>
+    </div>
+  );
 };
+
+
