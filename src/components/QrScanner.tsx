c0324451830682c4dbaf9b5f9ac3b74e11c2966e
maxitrigo <maxitrigo@gmail.com>
2024-12-16 11:11:17 -0300

    scanner

--- src/components/QrScanner.tsx
+++ src/components/QrScanner.tsx
@@ -1,81 +1,11 @@
-// import { useEffect, useRef, useState } from "react";
-// import { Html5Qrcode } from "html5-qrcode";
+import { Html5Qrcode } from "html5-qrcode";
 import { authInfo, logTraining } from "../helpers/DataRequests";
 import { useDispatch } from "react-redux";
 import { setGymUser } from "../Redux/gymUserSlice";
 import ConfirmationCircle from "./ConfirmationCircle";
 
-import { Html5QrcodeScanner } from "html5-qrcode"
+// import { Html5QrcodeScanner } from "html5-qrcode"
 import { useEffect, useState } from "react"
-
-// const QRScanner: React.FC = () => {
-//   const [confirmed, setConfirmed] = useState<boolean>(false);
-//   const [scannerUsed, setScannerUsed] = useState<boolean>(false);
-//   const qrCodeRef = useRef<HTMLDivElement>(null);
-//   const dispatch = useDispatch();
-
-//   const handleScan = async (decodedText: string) => {
-//     try {
-//       let result;
-
-//       if (decodedText === "log-training") {
-//         const { token } = authInfo();
-//         result = await logTraining(token);
-//       } else if (decodedText) {
-//         const token = decodedText;
-//         result = await logTraining(token);
-//       }
-
-//       if (result && !result.error) {
-//         dispatch(setGymUser(result));
-//         setConfirmed(true);
-//       } else {
-//         setConfirmed(false);
-//       }
-//     } catch (error) {
-//       setConfirmed(false);
-//     } finally {
-//       setScannerUsed(true);
-//     }
-//   };
-
-//   useEffect(() => {
-//     const startQRScanner = async () => {
-//       if (qrCodeRef.current && !scannerUsed) {
-//         const html5QrCode = new Html5Qrcode("qr-reader");
-//         try {
-//           await html5QrCode.start(
-//             { facingMode: "environment" },
-//             { fps: 40, qrbox: { width: 250, height: 250 } },
-//             handleScan,
-//             () => {}
-//           );
-//         } catch (err) {
-//         }
-//       }
-//     };
-//     startQRScanner()
-//     return () => {
-//     };
-//   }, [scannerUsed]);
-
-//   return (
-//     <div className="bg-background w-full h-full text-white">
-//       {!scannerUsed ? (
-//         <div
-//           id="qr-reader"
-//           ref={qrCodeRef}
-//         ></div>
-//       ) : (
-//         <ConfirmationCircle confirmed={confirmed} />
-//       )}
-//     </div>
-//   );
-// };
-
-// export default QRScanner;
-
-
 
 const QRScanner = () => {
   const [scanResult, setScanResult] = useState()
@@ -83,24 +13,48 @@ const QRScanner = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const scanner = new Html5QrcodeScanner('reader', {
-      qrbox: {
-        width: 250,
-        height: 250,
-      },
-      fps: 40,
-    }, true )
+    // const scanner = new Html5QrcodeScanner('reader', {
+    //   qrbox: {
+    //     width: 250,
+    //     height: 250,
+    //   },
+    //   fps: 40,
+    // }, false )
     
-    const error = (err: any) => {
-      console.warn(err)
-    }
-    const success = (result: any) => {
+    // const error = () => {
+
+    // }
+    // const success = (result: any) => {
+    //   scanner.pause(true)
+    //   scanner.clear()
+    //   setScanResult(result)
+    //   handleScann(scanResult)
+    // }
+
+    // scanner.render(success,error)
+    
+    const successCallback = (result: any) => {
+      console.log('verificado')
       scanner.clear()
       setScanResult(result)
       handleScann(scanResult)
     }
 
-    scanner.render(success,error)
+    const scanner = new Html5Qrcode('reader')
+    // const cameras = Html5Qrcode.getCameras()
+    scanner.start(
+      {facingMode: 'user'}, 
+      {
+      qrbox: {
+        width: 250,
+        height: 250,
+      },
+      fps: 40,
+      },
+      successCallback,
+      () => {}
+    )
+
 
   },[])
 
